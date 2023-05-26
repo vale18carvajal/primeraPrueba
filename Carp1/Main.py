9eb465298dd295b8544783717892ac6c997e9e3f
“Valeria <valeriacarvajal1@outlook.com>
2023-05-26 14:26:20 -0600

    Código inicial del archivo

---
+++ Carp1/Main.py
@@ -0,0 +1,12 @@
+print("Hola Mundo!")
+#Voy a hacer una prueba de método
+
+
+def SumaDeNumeros (a,b):
+    return a + b
+
+
+num1= int(input("Ingrese un número"))
+num2= int(input("Ingrese otro número"))
+
+print("La suma de los números es: ",SumaDeNumeros (num1,num2))
