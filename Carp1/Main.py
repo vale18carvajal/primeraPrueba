27eec7c26ea3dd0baaec7a49f89e1f2caad8eb41
“Valeria <valeriacarvajal1@outlook.com>
2023-05-26 14:31:54 -0600

    Agregué un nuevo método de resta

--- Carp1/Main.py
+++ Carp1/Main.py
@@ -10,3 +10,10 @@ num1= int(input("Ingrese un número"))
 num2= int(input("Ingrese otro número"))
 
 print("La suma de los números es: ",SumaDeNumeros (num1,num2))
+
+#Agrego método de resta
+
+def RestaDeNumeros (a,b):
+    return a - b
+
+print("La resta de los números es: ",RestaDeNumeros (num1,num2))
